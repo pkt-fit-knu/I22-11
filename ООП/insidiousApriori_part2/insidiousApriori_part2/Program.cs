cd8f4c332e68e35048da6d028d8e5ee5ccec0e40
OstMaria <ghosthostost@gmail.com>
2016-05-12 15:55:49 +0300

    1

---
+++ ООП/insidiousApriori_part2/insidiousApriori_part2/Program.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace insidiousApriori_part2
+{
+    class Program
+    {
+      
+        class candidate
+        {
+            List<int> Itemset;
+            int Support;
+
+            public candidate(List<int> Itemset, int Sup)
+            {
+                this.Itemset = Itemset;
+                this.Support = Sup;
+            }
+
+
+            public void addItemset2(int i1, int i2)
+            {
+
+                this.Itemset.Add(i1);
+                this.Itemset.Add(i2);
+
+            }
+
+            public void print()
+            {
+                for (int i = 0; i < this.Itemset.Count; i++)
+                {
+                    Console.Write(this.Itemset[i]);
+                }
+                Console.WriteLine("");
+            } //+
+
+
+            public void support(int[,] A) // підтримка кандидата з будь-якою кільк елементів
+            {
+
+
+                for (int i = 0; i < A.GetLength(0); i++)
+                {
+                    int f = 0;
+                    for (int j = 0; j < this.Itemset.Count; j++)
+                    {
+                        if (A[i, this.Itemset[j]] == 1)
+                        {
+                            f++;
+                        }
+                    }
+
+                    if (f == this.Itemset.Count)
+                    {
+                        this.Support++;
+                    }
+                }
+
+
+            }
+
+            public bool del(int k)   //+
+            {
+                bool D = false;
+                if (this.Support < k)
+                {
+                    D = true;
+                }
+                for (int i = 0; i < this.Itemset.Count; i++)
+                {
+                    for (int j = 0; j < this.Itemset.Count; j++)
+                    {
+                        if ((this.Itemset[i] == this.Itemset[j]) && (i != j))
+                        {
+                            D = true;
+                        }
+                    }
+                }
+                return D;
+            }
+            public bool del2(candidate A)  //+  
+            {
+                bool D = false;
+                int r = 0; // кількість однакових ел. в кандидатах.
+                for (int i = 0; i < this.Itemset.Count; i++)
+                {
+                    for (int j = 0; j < A.Itemset.Count; j++)
+                    {
+                        if (this.Itemset[i] == A.Itemset[j])
+                        {
+                            r++;
+                        }
+                    }
+                }
+                if (r == this.Itemset.Count) { D = true; }
+                return D;
+            }
+
+            public int findr(candidate A)  //+  виводить кількість однакових елементів серед 2 кандидатів
+            {
+
+                int r = 0; // кількість однакових ел. в кандидатах.
+                for (int i = 0; i < this.Itemset.Count; i++)
+                {
+                    for (int j = 0; j < A.Itemset.Count; j++)
+                    {
+                        if (this.Itemset[i] == A.Itemset[j])
+                        {
+                            r++;
+                        }
+                    }
+                }
+                
+                return r;
+            }
+
+            public candidate newcandidat(candidate A, int[,] B) //СТВОРЕННЯ НОВОГО КАНДИДАТА+
+            {
+                candidate newc = new candidate(new List<int>(), 0);
+                for (int i = 0; i < this.Itemset.Count; i++)
+                {
+                    newc.Itemset.Add(this.Itemset[i]);
+                }
+                for (int i = 0; i < A.Itemset.Count; i++)
+                {
+                    newc.Itemset.Add(A.Itemset[i]);
+                }
+                newc.cleanItemset();
+                newc.support(B);
+                return newc;
+            }
+
+            public void cleanItemset() //clean для створ нового кандидата +
+            {
+                for (int i = 0; i < this.Itemset.Count; i++)
+                {
+                    for (int j = 0; j < this.Itemset.Count; j++)
+                    {
+                        if ((this.Itemset[i] == this.Itemset[j]) && (i != j))
+                        {
+                            this.Itemset.RemoveAt(j);
+                            j--;
+                        }
+                    }
+                }
+            }
+
+        }
+        
+
+            static  int[,] vvid(int n, int m) //+
+            {
+                ///
+                
+                int[,] tabl = new int[n, m];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        tabl[i, j] = 0;
+                    }
+                }
+                
+                    ///
+                    Console.WriteLine("Вас вiтає Пiдступний Апрiорi");
+                Console.WriteLine("Данні зчитаються з файлу 1.txt");
+                for (int i = 0; i < n; i++)
+                {
+                string Line = File.ReadLines("1.txt").Skip(i).First();
+                    string[] input = Line.Split(' ');
+                    for (int j = 0; j < input.Length; j++)
+                    {
+                        int f = int.Parse(input[j]);
+                        tabl[i, f] = 1;
+
+                    }
+                }
+               
+                
+                    return tabl;
+            }
+
+            static void apriori(int[,] A, int m, int k)
+            {
+                int u = 0;
+                List<candidate> set1 = new List<candidate>();
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        set1.Add(new candidate(new List<int>(), 0));
+                        set1[u].addItemset2(i, j);
+                        set1[u].support(A);
+                        u++;
+                    }
+                }
+                cleanset(set1, k);
+
+                List<List<candidate>> sets = new List<List<candidate>>();
+                sets.Add(set1);
+                if (set1.Count == 0) { Console.WriteLine("Fatal system error"); }
+                else
+                {
+                    for (int i = 0; i < m; i++)
+                    {
+
+                        sets.Add(Generaticnewset(sets, A, k));
+                        if (sets[sets.Count-1].Count == 0)
+                        {
+                            print(sets[sets.Count-2]);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            static void print(List<candidate> set)
+            {
+                Console.WriteLine("Результат");
+                for (int i = 0; i < set.Count; i++)
+                {
+                    set[i].print();
+                }
+                Console.WriteLine("End");
+            }
+
+            static List<candidate> Generaticnewset(List<List<candidate>> sets, int[,] A, int k)
+            {
+                int N = sets.Count;
+                List<candidate> startset = sets[N - 1];
+                
+                List<candidate> finset = new List<candidate>();
+                for (int i = 0; i < startset.Count; i++)
+                {
+                    for (int j = 0; j < startset.Count; j++)
+                    {
+                       if (startset[i].findr(startset[j]) == N)
+                        {
+                         
+                         finset.Add(startset[i].newcandidat(startset[j],A));
+                        }
+                    }
+                }
+                cleanset(finset, k);
+                return finset;
+
+          
+            }
+           
+
+            static void cleanset(List<candidate> set, int k)
+            {
+                for (int i = 0; i < set.Count; i++)
+                {
+                    bool D;
+                    D = set[i].del(k);
+                    if (D == true)
+                    {
+                        set.RemoveAt(i);
+                        i--;
+                    }
+
+                }
+                for (int i = 0; i < set.Count; i++)
+                {
+                    for (int j = 0; j < set.Count; j++)
+                    {
+                        if ((set[i].del2(set[j]) == true) && (i != j))
+                        {
+                            set.RemoveAt(j);
+                            j--;
+                        }
+                    }
+                }
+                
+
+            } //+
+
+            static void Main(string[] args)
+            {
+                int k = 3;
+                int n = 10;
+                int m = 10;
+                apriori(vvid(n, m), m,k);
+                System.Threading.Thread.Sleep(500);
+                Console.WriteLine("All right to the creators");
+                System.Threading.Thread.Sleep(400);
+                Console.WriteLine("Made with investors support");
+                System.Threading.Thread.Sleep(300);
+                Console.WriteLine("We accept no liability for this material");
+                System.Threading.Thread.Sleep(300);
+                Console.WriteLine("Thank you for using just our program");
+                Console.ReadKey();
+            }
+        }
+    }
+
---
+++ ООП/insidiousApriori_part2/insidiousApriori_part2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("insidiousApriori_part2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("insidiousApriori_part2")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("fda1bf95-6db1-4b60-9d64-e17f49601534")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
